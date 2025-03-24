6bd743618451ed3cdb0e8d2b28feacf8eea32693
masbudicahngawi <masbudicahngawi@gmail.com>
2025-03-24 21:59:21 +0700

    mengganti endpoin api ke geni dan sedikit penambahan css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect,} from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom"
+import './Home.css';
 
 export default function Home(){
 	const [books, setBooks] = useState([]);
@@ -9,9 +10,10 @@ export default function Home(){
 	useEffect(()=>{
 		const getData = async ()=>{
 			try{
-				const hasil = await axios.get("http://localhost:4044/books");
+				// const hasil = await axios.get("http://localhost:4044/books");
+				const hasil = await axios.get("https://geni.masbudinulis.my.id/api/all");
 				console.log(hasil);
-				setBooks(hasil.data);
+				setBooks(hasil.data.data);
 			}catch(err){
 				console.log(err);
 			}
@@ -21,11 +23,11 @@ export default function Home(){
 	},[]);
 
 	return(
-		<div>
-			{books.map(satuan=>(
-				<div key={satuan.id}>
-					<h3 >{satuan.judul}</h3>
-					<p><i>{satuan.pengarang}</i></p>
+		<div className='container'>
+			{books.map((satuan, indeks)=>(
+				<div key={satuan.id} className='kotak'>
+					<h3 >{indeks + 1} - {satuan.nama}</h3>
+					<p><i>{((satuan.deskripsi == null) || (satuan.deskripsi =="")) ? 'No Description ...' : satuan.deskripsi}</i></p>
 				</div>))}
 			<hr/>
 			<Link to="/add">Add</Link>
